a4873a3c29e3458aeb1c70933dc9d5592360a24c
Eugene Lefter <eugen.lefter@gmail.com>
2019-07-19 16:01:18 -0400

    app cookie session

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
 require('./models/User');
 require('./services/passport');
 mongoose.connect(keys.mongoDB);
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        key: [keys.cookieKey]
+    })
+)
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
